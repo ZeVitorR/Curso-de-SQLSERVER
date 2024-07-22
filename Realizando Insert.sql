c6998d3a6b224ca7fa59d19be8d0237af71e61f6
José Vitor <jose.vitor@thomasicamargo.local>
2024-07-22 15:51:48 -0300

    Realizando insert

---
+++ Realizando Insert.sql
@@ -0,0 +1,19 @@
+
+--REALIZANDO INSERT
+
+/* Novo suco:
+1040107
+Light 350 ml - Melancia
+Lata
+350 ml
+Melancia
+R$ 4,56*/
+
+INSERT INTO [TABELA DE PRODUTO] VALUES (
+	'1040107',
+	'Light 350 ml - Melancia',
+	'Lata',
+	'350 ml',
+	'Melancia',
+	4.56
+);
